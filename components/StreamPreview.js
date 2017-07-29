856f761c608ce65fa0d5f661caf3d55c7d7d60e1
Nikita <nikita@Nikitas-MacBook-Pro.local>
2017-07-29 23:57:58 +0600

    [+] game streams list

---
+++ components/StreamPreview.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { TouchableHighlight, Image, View, Text, StyleSheet } from 'react-native';
+
+const StreamPreview = ({ preiviewImageUrl, userAvatarUrl, streamerName, streamDescription, streamGame }) => (
+  <TouchableHighlight style={styles.container}>
+    <View>
+      <Image source={{ uri: preiviewImageUrl }} style={styles.previewImage} />
+      <View style={styles.description}>
+        <Image source={{ uri: userAvatarUrl }} style={styles.avatarImage} />
+        <View style={styles.descriptionText}>
+          <Text style={styles.name}>{streamerName}</Text>
+          <Text>{streamDescription}</Text>
+          <Text style={styles.streamGame}>{streamGame}</Text>
+        </View>
+      </View>
+    </View>
+  </TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  previewImage: {
+    flex: 1,
+    height: 180
+  },
+  avatarImage: {
+    height: 45,
+    width: 45,
+    borderRadius: 5
+  },
+  description: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingLeft: 5,
+    paddingBottom: 10,
+  },
+  descriptionText: {
+    marginLeft: 5
+  },
+  name: {
+    fontWeight: 'bold'
+  },
+  streamGame: {
+    fontSize: 12,
+    color: 'grey'
+  }
+});
+
+export default StreamPreview;
--- containers/GameStreams.js
+++ containers/GameStreams.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView, StyleSheet } from 'react-native';
 import { getStreamsOf } from '../helpers/api';
+import Spinner from '../components/Spinner';
+import StreamPreview from '../components/StreamPreview';
 
 class GameStreams extends React.Component {
   constructor(props) {
@@ -12,14 +14,41 @@ class GameStreams extends React.Component {
 
   componentDidMount() {
     getStreamsOf(this.props.gameName).then(
-      res => console.log(res),
+      res => this.setState({ streams: res.streams }),
       error => console.log(error)
-    )
+    );
   }
 
   render() {
-    return <Text>{this.props.gameName}</Text>
+    if (this.state.streams.length === 0) {
+      return <Spinner />
+    } else {
+      const streamsList = this.state.streams.map((stream, index) => {
+        const description = stream.channel.status.length > 28 ? `${stream.channel.status.substring(0, 40)}...` : stream.channel.status;
+        return <StreamPreview
+          key={index}
+          preiviewImageUrl={stream.preview.large}
+          userAvatarUrl={stream.channel.logo}
+          streamerName={stream.channel.display_name}
+          streamDescription={description}
+          streamGame={stream.game}
+        />
+      })
+      return (
+        <ScrollView style={styles.streamsList}>
+          {streamsList}
+        </ScrollView>
+      )
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  streamsList: {
+    marginTop: 10,
+    marginLeft: 10,
+    paddingRight: 10,
+  }
+});
 
 export default GameStreams;
